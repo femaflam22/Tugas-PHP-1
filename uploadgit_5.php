c291f66ee33d15912c63effd2c5604f717a69f8f
femaflam22 <63629593+femaflam22@users.noreply.github.com>
2020-09-10 11:57:54 +0700

    Add files via upload

---
+++ uploadgit_5.php
@@ -0,0 +1,60 @@
+<?php
+class Produk{
+
+    //ini property
+    public $namaBarang,
+            $merk,
+            $ukuran,
+            $kapasitas,
+            $harga;
+
+    //ini constructor dengan parameter yang memiliki nilai default
+    public function __construct($namaBarang="nama_barang",$merk="merk",$harga=0,$ukuran="0 CM",$kapasitas="-"){
+        $this->namaBarang = $namaBarang;
+        $this->merk = $merk;
+        $this->harga = $harga;
+        $this->ukuran = $ukuran;
+        $this->kapasitas = $kapasitas;
+    }
+
+    //ini method
+    public function getCetak()
+    {
+        return "$this->merk | $this->harga | $this->ukuran | $this->kapasitas | <hr>";
+    }
+
+    // mencetak lebih lengkap
+    public function cetakInfo(){
+        $str = " {$this->namaBarang} : {$this->getCetak()}";
+        return $str;
+    }
+}
+
+class Laptop extends Produk{
+    public function cetakInfo(){
+        $str = "Info Pemesanan {$this->namaBarang} : {$this->getCetak()}";
+        return "<h1>Jenis Laptop</h1> <br> 
+                $str";
+    }
+}
+
+class Aksesoris extends Produk{
+    public function cetakInfo(){
+        $str = "Info Pemesanan {$this->namaBarang} : {$this->getCetak()}";
+        return "<h1>Jenis Aksesoris</h1> <br> 
+                $str";
+    }
+}
+
+
+//ini object
+//mengisi parameter constructor
+$produk1 = new Aksesoris("Earphone", "Samsung", 50000, "45 CM");
+$produk2 = new Laptop("Laptop", "Dell", 4000000, "40 x 60 CM", "4GB");
+$produk3 = new Aksesoris("Tas Laptop", "Dell", 35000, "40 x 60 CM", "-");
+
+echo $produk1->cetakInfo() . "<br>";
+echo $produk2->cetakInfo() . "<br>";
+echo $produk3->cetakInfo() . "<br>";
+
+?>
